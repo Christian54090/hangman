d0f38cb145a4c9641ce08f3c9d4062d114da3e27
Christian Sanders <christian.sanders@live.com>
2018-08-08 18:09:08 -0700

    Add functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,39 +1,79 @@
 class Hangman
 
-  def initialize(player1 = 'AI', player2 = 'AI')
-    @player1 = player1; @player2 = player2
-    words = File.read "5desk.txt"
-    word = words.split.select{|w| w.size.between?(5,12) }
-    GameEngine.new(word[rand(0..word.length)])
+  def initialize(player1 = 'AI',player2 = 'AI')
+    Player.new(player1, player2)
+    wds = File.read("5desk.txt").split
+    GameEngine.new(wds.select{|w| w.size.between?(5,12)}[rand(0..wds.length)])
   end
 
   class GameEngine
     def initialize(word)
-      @hidden_word = (word)
-      puts @hidden_word
+      $hidden_word = (word)
+      $word_hidden = $hidden_word.chars{|w| w = '_'}
     end
 
     def end_game(exit_text = "")
-      puts exit_text
-      exit(0)
+      puts exit_text; exit(0)
     end
   end
 
   class Player
-  end
 
-  class Gameplay
-    @incorrect_guesses = 0
-    @used_letters = ""
-  end
+    def initialize(player_one, player_two)
+      @player_one = player_one; @player_two = player_two
+    end
 
-  def board
-    puts %Q{
-      x more incorrect guesses until failure!
-      used letters = ""
+    @wrongs = 0; @used_chars = ""; @alph = ('a'..'z').to_a; @turns = 1
 
-      _____ _______ _____
-    }
+    @board = %Q{
+        #{8 - @wrongs} more incorrect guesses until failure!
+        used letters = #{@used_chars}
+
+        #{$word_hidden}
+      }
+
+    while @wrongs < 8 do
+      if @turns % 2
+        @player_two == 'AI' ? Player.ai : Player.human
+      else
+        @player_one == 'AI' ? Player.ai : Player.human
+      end
+      @turns += 1
+    end
+
+    def Player.ai
+      puts "I am a robot"
+      pick = @alph.select{|l| !(@used_chars.include?(l))}[rand(0..pick.length)]
+      if $hidden_word.include?(pick)
+        $word_hidden.chars do |c|
+          if $hidden_word.each_index.select{|i| $hidden_word[i] == pick}.include?($word_hidden.index(c))
+            c = pick; @used_chars << pick
+          end
+        end
+        exit_game("Success!") if !($word_hidden.include?('_'))
+      else
+        @used_chars << pick; puts @board; @wrongs += 1
+      end
+    end
+
+    def Player.human
+      puts "I am a human"
+      p '> '; pick = $stdin.gets.chomp
+
+      if @used_chars.include?(pick)
+        puts "That letter has already been chosen"; human
+      elsif !(@used_chars.include?(pick)) && $hidden_word.include?(pick)
+        $word_hidden.chars do |c|
+          if $hidden_word.each_index.select{|i| $hidden_word[i] == pick}.include?($word_hidden.index(c))
+            c = pick; @used_chars << pick
+          end
+        end
+        exit_game("Success!") if !($word_hidden.include?('_'))
+      elsif !(@used_chars.include?(pick)) && !($hidden_word.include?(pick))
+        @used_chars << pick; puts @board; @wrongs += 1
+      end
+
+    end
   end
 end
 
